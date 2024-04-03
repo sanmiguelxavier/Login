19a17db32564839b484b9d7554ad8f7ff65debbb
sanmiguelxavier <165364927+sanmiguelxavier@users.noreply.github.com>
2024-04-03 11:45:34 +0800

    Add files via upload

---
+++ Login/Login/Program.cs
@@ -0,0 +1,37 @@
+﻿class Login
+{
+    static void Main(string[] args)
+    {
+        string correctUsername = "Admin";
+        string correctPassword = "Admin1234!";
+        int maxAttempts = 3;
+        int attempts = 0;
+
+        System.Console.WriteLine("Login");
+
+        while (attempts < maxAttempts)
+        {
+            System.Console.Write("Enter your username: ");
+            string username = System.Console.ReadLine();
+
+            System.Console.Write("Enter your password: ");
+            string password = System.Console.ReadLine();
+
+            if (username == correctUsername && password == correctPassword)
+            {
+                System.Console.WriteLine("Login successful!");
+                break;
+            }
+            else
+            {
+                System.Console.WriteLine("Incorrect username or password. Please try again.");
+                attempts++;
+            }
+        }
+
+        if (attempts == maxAttempts)
+        {
+            System.Console.WriteLine("Maximum login attempts reached. Please try again later.");
+        }
+    }
+}
